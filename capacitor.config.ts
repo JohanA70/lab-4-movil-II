e55ab891449109d65b4f72e2bd52cc5a2eb2517d
JohanA70 <joalvarezor@est.utn.ac.cr>
2025-03-12 20:20:37 -0600

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oneSignal-P17',
+  webDir: 'dist'
+};
+
+export default config;
